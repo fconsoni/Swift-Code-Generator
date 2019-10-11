dce984aff9256d9797ebdb379237b4da6849021d
Franco Consoni <franco@goprebo.com>
2019-10-11 11:35:36 -0300

    fixed files

--- simpleStruct.swift
+++ simpleStruct.swift
@@ -1,29 +1,13 @@
-struct Trailer: Contentable {
-    let id: String
-    let title: String
-    let images: [ContentImage]
-    var contentType: ContentType
-    var bookmarkPosition: Int = 0
-    let duration: Int
-    let url: PlayerUrl
-    let releasedYear: Int
-    var resourceId: String = ""
-    var isFavorite: Bool = false
-    var hd: Bool = false
-    var cc: Bool = false
-    var isCatchup: Bool = true
-    var description: String = ""
-    var genre: String = ""
-    var credits: Credit
-    var parentalLevel: ParentalLevel
-    var dueDate: TimeInterval?
-    var trailerId: String?
+struct DetectiveTutorialModel {
+    let image: UIImage
+    let title: NSAttributedString
+    let description: NSAttributedString
+    let imageWithCorners: Bool
     
-    func getId() -> String {
-        return id
-    }
-    
-    func getContentType() -> ContentType {
-        return contentType
+    init(image: UIImage, title: NSAttributedString, description: NSAttributedString, imageWithCorners: Bool = false) {
+        self.image = image
+        self.title = title
+        self.description = description
+        self.imageWithCorners = imageWithCorners
     }
 }
